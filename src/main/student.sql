037f2e4729c3d659ee061557581d7ee8133348a4
RsvsNeeraj0306 <141002718+RsvsNeeraj0306@users.noreply.github.com>
2023-08-30 11:25:41 +0530

    Add files via upload

---
+++ src/main/student.sql
@@ -0,0 +1,13 @@
+use studentInfo;
+
+ 
+
+create Table student (
+student_id INT PRIMARY KEY AUTO_INCREMENT,
+studentName varchar(50),
+studentScore INT,
+studentRank INT);
+
+ 
+
+select * from student;
